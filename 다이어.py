456d1e58cfead4034a684f2192007f9e1cb83b32
hgy9709 <hgy9709@naver.com>
2016-03-30 01:25:27 +0900

    ㅇ

---
+++ 다이어.py
@@ -0,0 +1,14 @@
+@startuml
+start
+:set how many turns;
+:set how much to grow bigger;
+:set angle;
+:set start size;
+repeat
+if (i is divided by 2) then
+    :grow bigger;
+endif
+:draw;
+repeat while(turns)
+stop
+@enduml
