21c342b68b7907e5489a72c586e75b09d3e66f1e
hgy9709 <hgy9709@naver.com>
2016-04-11 16:55:09 +0900

    W6main_3

---
+++ W6main_3.py
@@ -0,0 +1,16 @@
+﻿x=list()
+def sumList(x):
+        for i in range(0,1001):
+                if(i%4==0 and i%5>0):
+                        x.append(i)
+        sum=0
+        for g in range(0,len(x)):
+                sum=sum+x[g]
+        return sum
+        print sum
+def lab6():
+        print sumList(x)
+lab6()
+
+def main():
+    lab6()
