7d5eb01eb4526ed9c43a653b94987d2575f32283
hgy9709 <hgy9709@naver.com>
2016-04-18 16:56:03 +0900

    Update W7main (3).py

--- W7main (3).py
+++ W7main (3).py
@@ -15,5 +15,21 @@ def lab7a():
     mytrack=drawSquareAt(size, pos)
     print mytrack
     
+tracks=dict()
+tracks=({1:(200,0),2:(200,200),3:(0,200),4:(0,0)})
+def drawSquareFrom():
+ for i in range(1,5):
+  t1.goto(tracks[i])
+  
+def lab7b():
+t1.penup()
+t1.goto(0,0)
+t1.pendown()
+drawSquareFrom()
+
 def main():
     lab7a()
+    lab7b()
+    
+main()
+wn.exitonclick()
