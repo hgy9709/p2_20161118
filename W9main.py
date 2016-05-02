86cac2fd47e989b72414bf52ed05b9742d267263
hgy9709 <hgy9709@naver.com>
2016-05-02 15:46:16 +0900

    w9main.py

---
+++ W9main.py
@@ -0,0 +1,17 @@
+﻿sentence="Sangmyung Univ., Hongji-dong, Jongno-gu, Seoul, Korea"
+allchars=list(sentence)
+    
+d=dict()
+for c in allchars:
+    if c not in d:
+        d[c]=1
+    else:
+        d[c]=d[c]+1
+
+%matplotlib
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.bar(range(len(d)),d.values(), align='center')
+plt.xticks(range(len(d)), list(d.keys()))
+plt.show()
