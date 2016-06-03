af86631fc01370109bf4e725c9315d245a3ddb4c
hgy9709 <hgy9709@naver.com>
2016-06-03 20:15:38 +0900

    강아지

---
+++ dog.py
@@ -0,0 +1,37 @@
+﻿class Dog(object): 
+    def __init__(self,name): 
+        self.name=name 
+    def Name(self): 
+        print 'my dog name ',self.name 
+    def talk(self): 
+        print 'dogs say : mung mung' 
+
+class Shihtu(object): 
+    def __init__(self,name): 
+        self.name=name 
+    def talk(self): 
+        print 'ShihTzu : si si' 
+ 
+class Maltse(object): 
+    def __init__(self,name): 
+        self.name=name 
+    def talk(self): 
+        print 'Maltese : mal mal' 
+ 
+def DogsSay(): 
+    mydog=Dog('dogs') 
+    mydog.talk() 
+    myshihtu=Shihtu('Shihtu') 
+    myshihtu.talk() 
+    mymaltse=Maltse('Maltse') 
+    mymaltse.talk() 
+ 
+def lab14(): 
+    DogsSay() 
+ 
+def main(): 
+    lab14() 
+ 
+if __name__=="__main__": 
+ main()  
+  
